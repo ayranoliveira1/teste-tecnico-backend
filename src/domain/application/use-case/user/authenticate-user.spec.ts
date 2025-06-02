bd4905fae057473acb5fe84b95b02e688132ec09
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-02 18:10:33 -0300

    feat: add authenticate user use case and test unit

---
+++ src/domain/application/use-case/user/authenticate-user.spec.ts
@@ -0,0 +1,66 @@
+import { FakerHasher } from 'test/cryptography/faker-hasher'
+import { FakerEncrypter } from 'test/cryptography/faker-encrypter'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { InMemoryUserRepository } from 'test/repositories/in-memory-user-repository'
+import { AuthenticateUserUseCase } from './authenticate-user'
+import { makeUser } from 'test/factories/make-user'
+
+let inMemoryUserRepository: InMemoryUserRepository
+let fakerHash: FakerHasher
+let fakerEcrypter: FakerEncrypter
+let sut: AuthenticateUserUseCase
+
+describe('Authenticate User', () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository()
+    fakerHash = new FakerHasher()
+    fakerEcrypter = new FakerEncrypter()
+    sut = new AuthenticateUserUseCase(
+      inMemoryUserRepository,
+      fakerHash,
+      fakerEcrypter,
+    )
+  })
+
+  it('should be able to Authenticate a user', async () => {
+    const user = makeUser({
+      password: await fakerHash.hash('any_password'),
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      email: user.email,
+      password: 'any_password',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual(
+      expect.objectContaining({
+        token: expect.any(String),
+        refreshToken: expect.any(String),
+        user: expect.objectContaining({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+        }),
+      }),
+    )
+  })
+
+  it('should hash user password upon authenticate', async () => {
+    const user = makeUser()
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      email: user.email,
+      password: 'teste3009211',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidCredentialsError)
+  })
+})
---
+++ src/domain/application/use-case/user/authenticate-user.ts
@@ -0,0 +1,64 @@
+import { Either, left, right } from '@/core/either'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { HashCompare } from '../../cryptography/hash-compare'
+import { Encrypter } from '../../cryptography/encrypter'
+import { Injectable } from '@nestjs/common'
+import { UserRepository } from '../../repositories/user-repository'
+import { User } from '@/domain/enterprise/entities/user'
+
+interface AuthenticateUserUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateUserUseCaseResponse = Either<
+  InvalidCredentialsError,
+  {
+    user: User
+    token: string
+    refreshToken: string
+  }
+>
+
+@Injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    private userRepository: UserRepository,
+    private hashCompare: HashCompare,
+    private encrypt: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      return left(new InvalidCredentialsError())
+    }
+
+    const passwordIsValid = await this.hashCompare.compare(
+      password,
+      user.password,
+    )
+
+    if (!passwordIsValid) {
+      return left(new InvalidCredentialsError())
+    }
+
+    const token = await this.encrypt.encrypt({
+      sub: user.id.toString(),
+    })
+
+    const refreshToken = await this.encrypt.encryptRefresh({
+      sub: user.id.toString(),
+    })
+
+    return right({
+      user,
+      token,
+      refreshToken,
+    })
+  }
+}
