ea5ebbf6e75b7171d5c16b3ce7b4e995734a1d36
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-02 20:18:06 -0300

    feat: add task repository

---
+++ src/domain/application/repositories/task-repository.ts
@@ -0,0 +1,8 @@
+import { Task } from '@/domain/enterprise/entities/task'
+
+export abstract class TaskRepository {
+  abstract findById(id: string): Promise<Task | null>
+  abstract create(task: Task): Promise<void>
+  abstract update(task: Task): Promise<void>
+  abstract delete(task: Task): Promise<void>
+}
