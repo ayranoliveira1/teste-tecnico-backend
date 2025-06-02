05922b2b3796fdaa2025cc483e43cd8faa2e111d
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-02 20:17:57 -0300

    feat: add task entity

---
+++ src/domain/enterprise/entities/task.ts
@@ -0,0 +1,80 @@
+import { Optional } from '@/core/@types/options'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export interface TaskProps {
+  title: string
+  description?: string
+  status: 'pending' | 'completed'
+  dueDate?: Date
+  userId: UniqueEntityId
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class Task extends Entity<TaskProps> {
+  get title() {
+    return this.props.title
+  }
+
+  get description() {
+    return this.props.description
+  }
+
+  get status() {
+    return this.props.status
+  }
+
+  get dueDate() {
+    return this.props.dueDate
+  }
+
+  get userId() {
+    return this.props.userId
+  }
+  s
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.touch()
+  }
+
+  set description(description: string | undefined) {
+    this.props.description = description
+    this.touch()
+  }
+
+  set status(status: 'pending' | 'completed') {
+    this.props.status = status
+    this.touch()
+  }
+
+  set dueDate(dueDate: Date | undefined) {
+    this.props.dueDate = dueDate
+    this.touch()
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(props: Optional<TaskProps, 'createdAt'>, id?: UniqueEntityId) {
+    const task = new Task(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return task
+  }
+}
