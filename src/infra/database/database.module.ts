a9f6fe7f7d0720217eaf228805310379725b39f1
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-03 15:59:55 -0300

    feat: add prisma task repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { UserRepository } from '@/domain/application/repositories/user-repository'
 import { PrimsaUserRepository } from './prisma/repositories/prisma-user-repository'
+import { TaskRepository } from '@/domain/application/repositories/task-repository'
+import { PrismaTaskRepository } from './prisma/repositories/prisma-task-repository'
 
 @Module({
   providers: [
@@ -10,7 +12,11 @@ import { PrimsaUserRepository } from './prisma/repositories/prisma-user-reposito
       provide: UserRepository,
       useClass: PrimsaUserRepository,
     },
+    {
+      provide: TaskRepository,
+      useClass: PrismaTaskRepository,
+    },
   ],
-  exports: [PrismaService, UserRepository],
+  exports: [PrismaService, UserRepository, TaskRepository],
 })
 export class DataBaseModule {}
---
+++ src/infra/database/prisma/repositories/prisma-task-repository.ts
@@ -0,0 +1,80 @@
+import { TaskRepository } from '@/domain/application/repositories/task-repository'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { Task } from '@/domain/enterprise/entities/task'
+import { PrismaTaskMapper } from '../mappers/prisma-task-mapper'
+
+@Injectable()
+export class PrismaTaskRepository implements TaskRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string) {
+    const task = await this.prisma.task.findUnique({
+      where: {
+        id,
+        isDeleted: false,
+      },
+    })
+
+    if (!task) return null
+
+    return PrismaTaskMapper.toDomain(task)
+  }
+
+  async findMany(userId: string) {
+    const tasks = await this.prisma.task.findMany({
+      where: {
+        userId,
+        isDeleted: false,
+      },
+    })
+
+    if (!tasks) return null
+
+    return tasks.map(PrismaTaskMapper.toDomain)
+  }
+
+  async findManyByStatus(userId: string, status: 'pending' | 'completed') {
+    const tasks = await this.prisma.task.findMany({
+      where: {
+        userId,
+        isDeleted: false,
+        status,
+      },
+    })
+
+    if (!tasks) return null
+
+    return tasks.map(PrismaTaskMapper.toDomain)
+  }
+
+  async create(task: Task) {
+    const data = PrismaTaskMapper.toPrisma(task)
+
+    await this.prisma.task.create({
+      data,
+    })
+  }
+
+  async update(task: Task) {
+    const data = PrismaTaskMapper.toPrisma(task)
+
+    await this.prisma.task.update({
+      where: {
+        id: task.id.toString(),
+      },
+      data,
+    })
+  }
+
+  async delete(task: Task) {
+    await this.prisma.task.update({
+      where: {
+        id: task.id.toString(),
+      },
+      data: {
+        isDeleted: true,
+      },
+    })
+  }
+}
